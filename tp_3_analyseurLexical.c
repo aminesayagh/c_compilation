db9ffd1caf55c2398b6d3804973af066372a9536
aminesayagh <aminesayagh1997@gmail.com>
2023-01-01 10:18:22 +0100

    add commentaire

--- tp_3_analyseurLexical.c
+++ tp_3_analyseurLexical.c
@@ -2,44 +2,56 @@
 #include <stdio.h>
 #include <string.h>
 
-int **saisirMatrice(int row, int col){
+// Cette fonction demande à l'utilisateur de saisir une matrice de taille row x col
+// et renvoie cette matrice sous forme de tableau à deux dimensions
+int **saisirMatrice(int row, int col) {
     int **matrice;
     matrice = (int**)malloc(row*sizeof(int*));
-    for(int i = 0; i < row; i++){
+
+    // Alloue de la mémoire pour chaque ligne de la matrice
+    for (int i = 0; i < row; i++) {
         matrice[i] = (int*)malloc(col*sizeof(int));
-        for(int j = 0; j < col; j++) {
-            printf("matrice[%d][%d] = ",i, j);
+
+        // Demande à l'utilisateur de saisir chaque élément de la matrice
+        for (int j = 0; j < col; j++) {
+            printf("matrice[%d][%d] = ", i, j);
             scanf("%d", &matrice[i][j]);
         }
     }
     return matrice;
 }
-void afficherMatrice(int **matrice, int row, int col){
-    for(int i = 0; i < row; i++){
-        for(int j = 0; j < col; j++){
-            printf("matrice[%d][%d] = %d \n",i, j, matrice[i][j]);
+
+// Cette fonction affiche la matrice passée en paramètre
+void afficherMatrice(int **matrice, int row, int col) {
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            printf("matrice[%d][%d] = %d \n", i, j, matrice[i][j]);
         }
     }
 }
 
+// Cette fonction demande à l'utilisateur de saisir un tableau de taille row
+// et renvoie ce tableau
 int *saisirTableau(int row) {
     int *tab = (int*)malloc(row*sizeof(int));
-    for(int i = 0;i < row; i++){
+    for (int i = 0; i < row; i++) {
         printf("Tab[%d] = ", i);
         scanf("%d", &tab[i]);
     }
     return tab;
 }
 
-int trace(int **matriceTransition, int *mot, int taille){
+// Cette fonction calcule la trace du mot passé en paramètre dans la matrice de transition
+// Elle renvoie -1 si le mot n'est pas reconnu par le langage, sinon elle renvoie l'état final
+int trace(int **matriceTransition, int *mot, int taille) {
     int trace = 0;
-    for(int i = 0; i < taille && trace != -1; i++) {
+    for (int i = 0; i < taille && trace != -1; i++) {
         trace = matriceTransition[trace][mot[i]];
-        printf("T[%d][%d] = %d\n" , trace, mot[i],trace);
+        printf("T[%d][%d] = %d\n", trace, mot[i], trace);
     }
     return trace;
-    
 }
+
 
 int main() {
 
